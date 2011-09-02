ebc079322bf38a2dfb46148cad230ef3eed90a3c
Ivan Alejandro <ivanalejandro0@yahoo.com.ar>
2011-09-02 12:47:03 -0300

    updated 'how to play'

--- QTabooWords.py
+++ QTabooWords.py
@@ -157,25 +157,17 @@ class QTabooWords(QtGui.QMainWindow, object):
     def _setReglas(self):
         self.reglas = """<big>
             <center><strong>REGLAS DEL JUEGO</strong></center><br />
-            <strong>N&uacute;mero de jugadores:</strong> De 6 a 30.<br />
-            <strong>Edad de los jugadores:</strong> A partir de los 12 a&#241;os.<br />
-            <strong>Duraci&oacute;n:</strong> El que se establesca, pero es mejor el que llegua a un numero determinado de tarjetas.<br />
-            <strong>Meta del juego:</strong> El equipo que adivine m&#225;s palabras en un tiempo preestablecido, gana m&#225;s tarjetas. El que tenga m&#225;s tarjetas al final del juego, ser&#225; el ganador.<br />
-            <strong>Material requerido</strong>: tarjetas, cada una con una palabra por definir y cuatro palabras prohibidas.<br />
-
+            <strong>N&uacute;mero de jugadores:</strong> De 4 a 30.<br />
+            <strong>Edad de los jugadores:</strong> A partir de los 12 a&ntilde;os.<br />
+            <strong>Duraci&oacute;n:</strong> El que se establezca o el que llega antes a un numero determinado de tarjetas.<br />
             <br /><br />
             <strong><u>Como Jugar:</u></strong>
             <ol>
-            <li>Organizar al total de jugadores en dos equipos. En grupos mayores a 15 , puede ser recomendable hacer tres o m&#225;s equipos.</li>
-            <li>Pedir que cada equipo elija un representante.</li>
-            <li>El representante del primer equipo pasa al frente, con 5 tarjetas. Un miembro del segundo equipo se para junto a &eacute;l para:</li>
-            <ul>
-            <li>Vigilar que no use ninguna de las palabras prohibidas al definir la palabra que le toc&oacute;.</li>
-            <li>Medir el tiempo. Cada equipo tendr&aacute; un minuto para descubrir la(s) palabra(s) que su representante defina.</li>
-            </ul>
-            <li>El representante del primer equipo tiene un minuto para parafrasear la palabra que aparece en may&#250;sculas en la parte superior de cada tarjeta, pero NO podr&#225; usar ninguna de las palabras incluidas en la misma tarjeta porque son tab&#250;.</li>
-            <li>El equipo tratar&#225; de adivinar la palabra. Si lo logra, obtendr&#225; esa tarjeta y el representante parafrasear&#225; la tarjeta siguiente. Continuar as&#237; hasta que termine su tiempo.</li>
-            <li>Al terminar el tiempo de un equipo continuar&#225; el otro siguiendo las mismas reglas.</li>
+            <li>Se dividen las personas en 2 equipos.</li>
+            <li>Cada equipo tiene un turno y 60 segundos para hacerse entender.</li>
+            <li>La persona elegida por el equipo de turno tiene que intentar hacerle a entender a sus compa&ntilde;eros la palabra dada como consigna en MAYUSCULAS sin decir las 5 palabras dadas como tab&uacute;es.</li>
+            <li>Del equipo contrario tiene que haber una persona controlando que al parafrasear no se diga ninguna las palabras prohibidas. (como restricci&oacute;n adicional se puede prohibir usar las palabras similares o de la misma familia que las de la tarjeta)</li>
+            <li>Si se adivina la palabra consigna en el tiempo dado el equipo gana la tarjeta, sino la gana el equipo contrario.</li>
             </ol>
             </big>
             """
